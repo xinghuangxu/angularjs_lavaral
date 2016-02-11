9cebfaf81d5837b6f9e67245e1f1de0ed48a6e4f
Leon Xu <xinghuangxu@gmail.com>
2016-02-10 18:07:37 -0600

    Tag Tree Select Refactoring

--- public/angular/ui/treeSelect.js
+++ public/angular/ui/treeSelect.js
@@ -22,7 +22,9 @@
     {
         return {
             restrict: 'EA',
-            scope: {},
+            scope: {
+                current: '=current'
+            },
             templateUrl: 'angular/ui/partial/_treeSelect.html?v=0',
             link: function (scope, element, attrs) {
                 //see if read only has been configured
@@ -66,6 +68,10 @@
                         }
                     }
                 }
+                
+                scope.$watch('current',function(newValue, oldValue){
+                   alert("Helo");
+                });
 
                 //traverse the elements in the tree to update the input box tags
                 var allEle = {};
@@ -104,7 +110,7 @@
             }
         };
     };
-    
+
     /**
      * Un-flatten tag array into a tree
      * 
@@ -114,16 +120,26 @@
      */
     function buildTree(tagsArray, slice) {
         var result = [];
+        var keyList = {};
         for (var i = 0; i < tagsArray.length; i++) {
-            var single = {};
-            single.id = tagsArray[i]['CategoryID'];
-            var path = tagsArray[i]['CategoryPath'];
-            var lastIndex = path.lastIndexOf('\\');
-            single.text = path.substring(lastIndex+1);
-            single.parent = tagsArray[i]['CategoryFatherID'];
-            if(single.parent == "")single.parent="#";
-            single.icon = "";
-            result.push(single);
+            if (tagsArray[i]['IsActive'] === "1") {
+                keyList[tagsArray[i]['CategoryID']] = true;
+            }
+        }
+        for (var i = 0; i < tagsArray.length; i++) {
+            if (tagsArray[i]['IsActive'] === "1") {
+                var single = {};
+                single.id = tagsArray[i]['CategoryID'];
+                single.text = tagsArray[i]['CategoryName'];
+                if (keyList[tagsArray[i]['CategoryFatherID']]) {
+                    single.parent = tagsArray[i]['CategoryFatherID'];
+                } else {
+                    single.parent = "#";
+                }
+                single.icon = "";
+//                single.state = {selected: true};
+                result.push(single);
+            }
         }
         return result;
     }
