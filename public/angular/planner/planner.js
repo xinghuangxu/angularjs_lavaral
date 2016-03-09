6cf142ea732476fba43b8c0861257e157fda5f62
Leon Xu <xinghuangxu@gmail.com>
2016-03-09 17:01:35 -0600

    Change Test Planner Panel Control to Multiseleciton box

--- public/angular/planner/planner.js
+++ public/angular/planner/planner.js
@@ -6,26 +6,26 @@
  * @date 2015-08-03
  *
  */
-(function() {
+(function () {
     'use strict';
     // Parent module, used in dependency injection and resolution
     angular
-        .module('spark.planner')
-        .controller('Main', Main)
-        .controller('Releases', Releases)
-        .controller('Panes', Panes);
+            .module('spark.planner')
+            .controller('Main', Main)
+            .controller('Releases', Releases)
+            .controller('Panes', Panes);
 
     Main.$inject = ['globalFilterService'];
     /**
      * Controller for the test planner page
      */
-    function Main (filter) {
+    function Main(filter) {
         var vm = this;
 
         vm.filter = filter;
     }
 
-    Releases.$inject =  [ '$scope', 'testplanSettingsService',  'releasesService', 'testStackService' ];
+    Releases.$inject = ['$scope', 'testplanSettingsService', 'releasesService', 'testStackService'];
 
     /**
      * Controller for handling the release selection for settings
@@ -35,7 +35,7 @@
      * @param {object} releases Service reference for fetching release data
      * @param {object} stacks   Service reference for fetching stack layer data
      */
-    function Releases ($scope, planSettings, releases, stacks) {
+    function Releases($scope, planSettings, releases, stacks) {
         var vm = this;
 
         vm.releases = releases.query();
@@ -45,98 +45,100 @@
         vm.data = vm.settings.data;
 
         // Watch for changes to the train and update the list of test stack layers
-        $scope.$watch(function() {
-                return vm.data.release_id;
-            },
-            function() {
-                if(vm.data.release_id) {
-                    planSettings.service.query({
-                        release: vm.data.release_id,
-                        stack: '',
-                        substack: ''
-                    },
-                    function(data) {
-                        if(data.length > 0)
-                            planSettings.data = data[0];
-                    });
+        $scope.$watch(function () {
+            return vm.data.release_id;
+        },
+                function () {
+                    if (vm.data.release_id) {
+                        planSettings.service.query({
+                            release: vm.data.release_id,
+                            stack: '',
+                            substack: ''
+                        },
+                                function (data) {
+                                    if (data.length > 0)
+                                        planSettings.data = data[0];
+                                });
+                    }
+
+                    // TODO: Call relevant data service when value changes
+                    vm.data.testplan_stack_id = null;
+                    vm.data.testplan_substack_id = null;
+
+                    if (vm.data.release_id)
+                        vm.stackLayers = stacks.stack.asOptions({release: vm.data.release_id});
                 }
-
-                // TODO: Call relevant data service when value changes
-                vm.data.testplan_stack_id = null;
-                vm.data.testplan_substack_id = null;
-
-                if(vm.data.release_id)
-                    vm.stackLayers = stacks.stack.asOptions({ release: vm.data.release_id});
-            }
         );
 
         // Watch for changes to the test stack layer and update the list of sub-layers
-        $scope.$watch(function() {
-                return vm.data.testplan_stack_id;
-            },
-            function() {
-                if(vm.data.testplan_stack_id) {
-                    planSettings.service.query({
-                        release: vm.data.release_id,
-                        stack: vm.data.testplan_stack_id,
-                        substack: ''
-                    },
-                    function(data) {
-                        if(data.length > 0) {
-                            planSettings.data = data[0];
-                            if(planSettings.data.boxcar) {
-                                planSettings.data.testplan_stack_id = planSettings.data.boxcar.id;
-                                planSettings.data.stack_name = planSettings.data.boxcar.Name;
-                            } else {
-                                planSettings.data.stack_name = planSettings.data.stack.name;
-                            }
-                        }
-                    });
+        $scope.$watch(function () {
+            return vm.data.testplan_stack_id;
+        },
+                function () {
+                    if (vm.data.testplan_stack_id) {
+                        planSettings.service.query({
+                            release: vm.data.release_id,
+                            stack: vm.data.testplan_stack_id,
+                            substack: ''
+                        },
+                                function (data) {
+                                    if (data.length > 0) {
+                                        planSettings.data = data[0];
+                                        if (planSettings.data.boxcar) {
+                                            planSettings.data.testplan_stack_id = planSettings.data.boxcar.id;
+                                            planSettings.data.stack_name = planSettings.data.boxcar.Name;
+                                        } else {
+                                            planSettings.data.stack_name = planSettings.data.stack.name;
+                                        }
+                                    }
+                                });
+                    }
+
+                    // TODO: Call relevant data service when value changes
+                    vm.data.testplan_substack_id = null;
+
+                    if (vm.data.testplan_stack_id)
+                        vm.subLayers = stacks.substack.asOptions({stack: vm.data.testplan_stack_id});
                 }
-
-                // TODO: Call relevant data service when value changes
-                vm.data.testplan_substack_id = null;
-
-                if(vm.data.testplan_stack_id)
-                    vm.subLayers = stacks.substack.asOptions({ stack: vm.data.testplan_stack_id});
-            }
         );
 
         // Wait for changes in the test plan substack
-        $scope.$watch(function() {
-                return vm.data.testplan_substack_id;
-            },
-            function() {
-                if(vm.data.testplan_substack_id) {
-                    planSettings.service.query({
+        $scope.$watch(function () {
+            return vm.data.testplan_substack_id;
+        },
+                function () {
+                    if (vm.data.testplan_substack_id) {
+                        planSettings.service.query({
                             release: vm.data.release_id,
                             stack: vm.data.testplan_stack_id,
                             substack: vm.data.testplan_substack_id
                         },
-                        function(data) {
-                            if(data.length > 0) {
-                                planSettings.data = data[0];
+                                function (data) {
+                                    if (data.length > 0) {
+                                        planSettings.data = data[0];
 
-                                if(planSettings.data.boxcar) {
-                                    planSettings.data.testplan_stack_id = planSettings.data.boxcar.id;
-                                    planSettings.data.stack_name = planSettings.data.boxcar.Name;
-                                } else {
-                                    planSettings.data.stack_name = planSettings.data.stack.name;
+                                        if (planSettings.data.boxcar) {
+                                            planSettings.data.testplan_stack_id = planSettings.data.boxcar.id;
+                                            planSettings.data.stack_name = planSettings.data.boxcar.Name;
+                                        } else {
+                                            planSettings.data.stack_name = planSettings.data.stack.name;
+                                        }
+
+                                        planSettings.data.substack_name = planSettings.data.substack.name;
+                                    }
                                 }
-
-                                planSettings.data.substack_name = planSettings.data.substack.name;
-                            }
-                        }
-                    );
+                        );
+                    }
                 }
-            }
         );
     }
+
+    Panes.$inject = ['$scope'];
 
     /**
      * Controller for handling the panels which show the actual content in the planner
      */
-    function Panes () {
+    function Panes($scope) {
         var vm = this;
 
         vm.order = 'order';
@@ -147,29 +149,48 @@
             placeholder: 'pane panel panel-primary panel-placeholder'
         };
 
-        vm.active = function(item) { return item.active; };
+        vm.active = function (item) {
+            return item.active;
+        };
 
         vm.paneGroups = [
-             {
-                 label: 'What',
-                 name: 'what',
-                 order: 0
-             },
-             {
-                 label: 'Where',
-                 name: 'where',
-                 order: 1
-             },
-             {
-                 label: 'When',
-                 name: 'when',
-                 order: 2
-             }
-         ];
+            {
+                label: 'What',
+                name: 'what',
+                order: 0
+            },
+            {
+                label: 'Where',
+                name: 'where',
+                order: 1
+            },
+            {
+                label: 'When',
+                name: 'when',
+                order: 2
+            }
+        ];
 
+        //multi select configuration and watch events
+        vm.selectedPanes = ["High Level scope", "Test Plan"];
+        $scope.$watch(
+                function () {
+                    return vm.selectedPanes;
+                },
+                function () {
+                    for (var i = 0; i < vm.panes.length; i++) {
+                        var isSelected = (vm.selectedPanes.indexOf(vm.panes[i].label) > -1);
+                        if (isSelected) {
+                            vm.panes[i].active = true;
+                        } else {
+                            vm.panes[i].active = false;
+                        }
+                    }
+                }
+        );
         // Setup the defaults and configuration for each pane
         // Each template must contain a reference to the correct controller for that pane
-        vm.panes =  [
+        vm.panes = [
             {
                 label: "High Level scope",
                 template: "angular/planner/boxcar/boxcar.tpl.html",
@@ -241,7 +262,7 @@
                 order: 8,
                 active: false,
                 group: 'what'
-            }, 
+            },
             {
                 label: "RCCA",
                 //template: "planner/weblab/weblab.tpl.html",
