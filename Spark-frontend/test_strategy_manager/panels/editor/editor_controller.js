3264881031f0d80ff30b8d449b2bc3892951b377
rameez aijaz <sxaijaz@wichita.edu>
2016-04-11 16:13:30 -0500

    commented out the SIS service calling code in test strategy editor

--- Spark-frontend/test_strategy_manager/panels/editor/editor_controller.js
+++ Spark-frontend/test_strategy_manager/panels/editor/editor_controller.js
@@ -7,14 +7,17 @@
         .controller('strategyEditorController',
             strategyEditorController);
 
-    strategyEditorController.$inject= ['$state'];
-    function strategyEditorController($state){
+    strategyEditorController.$inject= ['$state','SISService','SISSuggestionsValue'];
+    function strategyEditorController($state,SISService,SISSuggestionsValue){
 
         var strategyEditorCtlr =this;
+        strategyEditorCtlr.strategy={headline:null,impact_area:null,qlt_area:null,approach:null,goal:null, };
 
 
         strategyEditorCtlr.changeLeftPlugin=changeLeftPlugin;
         strategyEditorCtlr.isTabActive = isTabActive;
+        strategyEditorCtlr.editorValueChanged = editorValueChanged;
+
 
         function changeLeftPlugin (state_name){
 
@@ -24,7 +27,46 @@
         function isTabActive(state_name) {
             return $state.current.name.indexOf(state_name) > -1
         }
+        function editorValueChanged(){
+            var arg = {
+                'adviser_type': 'ts_adviser', // Keep this static for the Test Strategy Manager
+                'StrategyHeadline': strategyEditorCtlr.strategy.headline,
+                'TestStrategy': 'description contents',
+                'categories': [strategyEditorCtlr.strategy.impact_area,
+                               strategyEditorCtlr.strategy.qlt_area,
+                               strategyEditorCtlr.strategy.approach],
+                'num_ts': 10,    // Leave at 10 as default for now. Should be dealt with in configuration
+                'Override_knob_values': 'false' // Leave at 'false' as default for now.
+            };
+            console.log(arg);
+            SISSuggestionsValue.test_strategies=null;
+            SISSuggestionsValue.test_strategies =[{
+                "StrategyID": 4239,
+                "StrategyHeadline": "SAM-EF Pool Management: Secure Erase Drives?",
+                "rel": 0.4133198922545748
+            },{
+                "StrategyID": 4200,
+                "StrategyHeadline": "Storage System Failure",
+                "rel": 0.4133198922545748
+            },{
+                "StrategyID": 3135,
+                "StrategyHeadline": "ABC",
+                "rel": 0.4133198922545748
+            },{
+                "StrategyID": 4056,
+                "StrategyHeadline": "Error handling for missing/corrupted files in cloud storage account (metadata files and data objects)",
+                "rel": 0.4133198922545748
+            },{
+                "StrategyID": 4061,
+                "StrategyHeadline": "Error handling for cloud provider-specific errors",
+                "rel": 0.4133198922545748
+            }];
+            //SiS service is not working
+            // SISService.getSuggestedTestStrategies(arg).then(function(){
+            //
+            // });
 
+        }
 
 
     }
